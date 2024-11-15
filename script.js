d33ed36a32ce6bca044704a936466e4c361e3f22
anirudh <kanirudh678@gmail.com>
2024-11-15 12:52:05 +0530

    update in read me file

---
+++ script.js
@@ -0,0 +1,5 @@
+let buttonElement = document.getElementById("button")
+let counterElement = document.getElementById("counter")
+buttonElement.onclick = function () {
+    counterElement.textContent=" COMING SOON..."
+}
